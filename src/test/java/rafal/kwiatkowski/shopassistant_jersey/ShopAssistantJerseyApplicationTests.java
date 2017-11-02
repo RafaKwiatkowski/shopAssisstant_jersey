79eef75382faaa160cc222c8ff291b4ff1742947
Rafał Kwiatkowski <raf.kwiatkowski93@gmail.com>
2017-11-02 22:26:16 +0100

    initial commit

--- src/test/java/rafal/kwiatkowski/shopassistant_jersey/ShopAssistantJerseyApplicationTests.java
+++ src/test/java/rafal/kwiatkowski/shopassistant_jersey/ShopAssistantJerseyApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShopassistantJerseyApplicationTests {
+public class ShopAssistantJerseyApplicationTests {
 
 	@Test
 	public void contextLoads() {
