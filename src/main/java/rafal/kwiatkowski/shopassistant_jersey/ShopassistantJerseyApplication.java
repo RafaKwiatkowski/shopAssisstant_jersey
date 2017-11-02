d051973d8bcc1ce730f00bb1c7d189cc78e4daeb
Rafał Kwiatkowski <raf.kwiatkowski93@gmail.com>
2017-11-02 22:24:42 +0100

    initial commit

---
+++ src/main/java/rafal/kwiatkowski/shopassistant_jersey/ShopassistantJerseyApplication.java
@@ -0,0 +1,12 @@
+package rafal.kwiatkowski.shopassistant_jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopassistantJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopassistantJerseyApplication.class, args);
+	}
+}
---
+++ src/test/java/rafal/kwiatkowski/shopassistant_jersey/ShopassistantJerseyApplicationTests.java
@@ -0,0 +1,16 @@
+package rafal.kwiatkowski.shopassistant_jersey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopassistantJerseyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
